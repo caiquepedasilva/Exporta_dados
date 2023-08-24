14b7a18b2c514f64cb53efa01dc3c4b7c7435b56
caiquepedasilva <caiquepe.dasilva@gmail.com>
2023-08-24 07:59:16 -0300

    Corrigindo a o arquivo teste para fazer a leitura

--- a.py
+++ a.py
@@ -1,26 +1,36 @@
 import PyPDF2
-import pandas as pd
+import openpyxl
 
-def extract_table_from_pdf(pdf_path):
+def extract_tables_from_pdf(pdf_path):
+    tables = []
     pdf_text = ""
+    
     with open(pdf_path, 'rb') as pdf_file:
         pdf_reader = PyPDF2.PdfReader(pdf_file)
         for page_num in range(len(pdf_reader.pages)):
             pdf_text += pdf_reader.pages[page_num].extract_text()
-    return pdf_text
+    
+    # Assume que cada linha está separada por uma quebra de linha
+    lines = pdf_text.split('\n')
+    
+    for line in lines:
+        table_row = line.split()  # Dividir a linha em colunas
+        tables.append(table_row)
+    
+    return tables
 
-pdf_text = extract_table_from_pdf('Pasta1.pdf')
-print(pdf_text)
+def export_to_excel(tables, excel_path):
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    
+    for table_row in tables:
+        sheet.append(table_row)  # Adicionar cada linha como uma nova linha na planilha
+        
+    workbook.save(excel_path)
 
-table_rows = pdf_text.split('\n')
-
-data = []
-for row in table_rows:
-     row_data = row.split()  
-     data.append(row_data)
-
-df = pd.DataFrame(data)
-print(df)
-
-excel_path = 'Novo.xlsx'
-df.to_excel(excel_path, index=False, header=True) 
+if __name__ == '__main__':
+    pdf_path = 'lista.pdf'  # Substitua pelo caminho do seu arquivo PDF
+    excel_path = 'tabela.xlsx'    # Substitua pelo caminho desejado para o arquivo Excel
+    
+    extracted_tables = extract_tables_from_pdf(pdf_path)
+    export_to_excel(extracted_tables, excel_path)
